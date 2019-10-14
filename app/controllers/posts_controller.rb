2d7c4cb5cf6500d81e447025cf13183d22bf18bc
MARCO MCNULTY <Marco@MARCOs-MacBook-Pro-2.local>
2019-10-14 12:53:14 +0100

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,30 +1,36 @@
 class PostsController < ApplicationController
-	def index
-		@posts = Post.all
-	end
+  def index
+    @posts = Post.all
+  end
 
-	def show
-		@post = Post.find(params[:id])
-	end
+  def show
+    @post = Post.find(params[:id])
+  end
 
-	def new
-		@post = Post.new
-		@categories = Category.all
-	end
+  def new
+    @post = Post.new
+    @categories = Category.all
+  end
 
-	def create
-		post = Post.create(params[:post])
-		redirect_to post_path(post)
-	end
+  def create
+    post = Post.create(post_params(:title, :description, :category_id))
+    redirect_to post_path(post)
+  end
 
-	def edit
-		@post = Post.find(params[:id])
-		@categories = Category.all
-	end
+  def edit
+    @post = Post.find(params[:id])
+    @categories = Category.all
+  end
 
-	def update
-		post = Post.find(params[:id])
-		post.update(params.require(:post))
-		redirect_to post_path(post)
-	end
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params(:title, :description, :category_id))
+    redirect_to post_path(post)
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:post).permit(*args)
+  end
 end
